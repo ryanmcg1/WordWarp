d3a68949ac87330dfb146cfc2ffc00cba9878c65
ryanmcg1 <ryan_mcguckin@hotmail.com>
2016-01-06 18:51:58 +0000

    webpage

---
+++ WordWarp/Webpage/a.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="a.aspx.cs" Inherits="a" %>
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server" enableviewstate="false">
+    <asp:Literal runat="server" ID="ltlTags" EnableViewState="false" />
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        User list:
+        <asp:TextBox ID="txtOnlineUsers" runat="server"></asp:TextBox>
+        <br />
+        Name On Report :
+        <asp:TextBox ID="txtReportName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" Text="Lookup" OnClick="btnSubmit_Click" />
+        <br />
+        
+            User: 
+            <asp:TextBox ID="txtResult" runat="server" EnableViewState="false"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ WordWarp/Webpage/a.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class a : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        txtResult.Text = "";
+        string[] onlineUserList = txtOnlineUsers.Text.ToString().Split('|');
+        char[] ReportName = txtReportName.Text.Replace("*", "").ToCharArray();
+
+        foreach (string user in onlineUserList)
+        {
+            int i = 0;
+            foreach (char letter in ReportName)
+            {
+                if (user.Contains(letter))
+                {
+                    i++;
+                }
+            }
+            if (i == ReportName.Length)
+            {
+
+                if (txtResult.Text == "")
+                {
+                    txtResult.Text = user;
+                }
+                else
+                {
+                    txtResult.Text = txtResult.Text + "," + user;
+                }
+            }
+
+
+        }
+
+        if (txtResult.Text == "")
+            txtResult.Text = "Not found!";
+    }
+}
