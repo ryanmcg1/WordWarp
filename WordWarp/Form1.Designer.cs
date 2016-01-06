a2d997fc82114616adb3564173bd10fb04f11051
ryanmcg1 <ryan_mcguckin@hotmail.com>
2016-01-06 18:27:26 +0000

    Initial

---
+++ WordWarp/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WordWarp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtOnlineUsers = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtReportName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtOnlineUsers
+            // 
+            this.txtOnlineUsers.Location = new System.Drawing.Point(67, 25);
+            this.txtOnlineUsers.Multiline = true;
+            this.txtOnlineUsers.Name = "txtOnlineUsers";
+            this.txtOnlineUsers.Size = new System.Drawing.Size(231, 102);
+            this.txtOnlineUsers.TabIndex = 0;
+            this.txtOnlineUsers.Text = "ryan | cat";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Lookup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtReportName
+            // 
+            this.txtReportName.Location = new System.Drawing.Point(121, 173);
+            this.txtReportName.Name = "txtReportName";
+            this.txtReportName.Size = new System.Drawing.Size(97, 20);
+            this.txtReportName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Online User list";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(121, 342);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Name from report";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "User";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 391);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtReportName);
+            this.Controls.Add(this.txtOnlineUsers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOnlineUsers;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtReportName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WordWarp/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordWarp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] onlineUserList = txtOnlineUsers.Text.ToString().Split('|');
+            char[] ReportName = txtReportName.Text.Replace("*", "").ToCharArray();
+
+            foreach(string user in onlineUserList)
+            {
+                int i = 0;
+                foreach(char letter in ReportName)
+                {
+                    if (user.Contains(letter))
+                    {
+                        i++;
+                    }
+                }
+                if (i == ReportName.Length)
+                {
+
+                    if (txtResult.Text == "")
+                    {
+                        txtResult.Text = user;
+                    }
+                    else
+                    {
+                        txtResult.Text = txtResult.Text + "," + user;
+                    }                    
+                }
+                    
+               
+            }
+
+            if (txtResult.Text == "")
+                txtResult.Text = "Not found!";
+        }
+
+    }
+}
